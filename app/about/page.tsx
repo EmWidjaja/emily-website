dd7c2f0cba7cc5320ed3b98f65276266807e7b04
Emily Widjaja <s3918260@student.rmit.edu.au>
2025-09-15 02:36:39 +1000

    created about page

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  return (
+    <main className="min-h-screen p-8 bg-gray-50">
+      <h1 className="text-4xl font-bold mb-4">About Me</h1>
+      <p className="text-lg text-gray-700">
+        Hi, I’m Emily. Welcome to my About page!  
+      </p>
+    </main>
+  );
+}
