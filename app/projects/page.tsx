6c3b3dc1e94da03552d3e1fa95b73ce337442cfe
Emily Widjaja <s3918260@student.rmit.edu.au>
2026-02-05 19:21:38 +1100

    OTL studios

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,7 +2,7 @@ export default function Projects() {
   return (
     <main className="p-8">
       <h1 className="text-4xl font-bold">Projects</h1>
-      <p className="mt-2 text-gray-600">Here are some of my works!</p>
+      <p className="mt-2 text-gray-600">OTL studios</p>
     </main>
   );
 }
