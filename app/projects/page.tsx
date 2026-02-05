6c5a238b7930fedd73b7c9e3c70eea5f2cdc98bc
Emily Widjaja <s3918260@student.rmit.edu.au>
2026-02-05 20:46:10 +1100

    trial 1

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,12 +1,30 @@
+import Link from 'next/link';
+
 export const metadata = {
-  title: 'OTL studios'
-}
+  title: 'OTL studios',
+  icons: {
+    icon: '',
+  },
+};
 
 export default function Projects() {
   return (
-    <main className="p-8">
-      <h1 className="text-4xl font-bold">Projects</h1>
-      <p className="mt-2 text-gray-600">OTL studios</p>
+    <main>
+      <nav className="flex items-center justify-between p-4"> 
+          <Link href="/">
+            <img src="/images/OTLlogo.jpeg" alt="Website Logo" width="120" height="60" />
+          </Link>
+
+          <div className="nav-links" id="navLinks">
+            <ul className="flex gap-4">
+              <li><Link href="/Shop">Shop</Link></li>
+              <li><Link href="/About">About</Link></li>
+              <li><Link href="/Read">Read</Link></li>
+              <li><Link href="/Events">Events</Link></li>
+              <li><Link href="/FAQ">FAQ</Link></li>
+            </ul>
+          </div>
+        </nav>
     </main>
   );
 }
